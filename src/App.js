30623f1678c806d3318baca6ccc319a06da368fb
mohit838 <mohitul449@gmail.com>
2022-06-04 22:27:41 +0600

    editing routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./pages/Home/Home/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Appointment from "./pages/Appointment/Appointment/Appoinment";
 import Login from "./pages/Login/Login/Login";
+import Register from "./pages/Login/Register/Register";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/home" element={<Home />}></Route>
           <Route path="/appointment" element={<Appointment />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </Router>
     </div>
