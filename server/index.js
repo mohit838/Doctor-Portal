b445070ab07b95370a2b23287c56334025b893d9
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-10-13 12:39:18 +0600

    All Parts together

---
+++ server/index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+// Middlewire
+app.use(cors());
+// We want JSON data from frontend
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.li30i.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const databaseName = client.db("doctorsDB");
+    const appointmentsInfo = databaseName.collection("appointments");
+
+    app.post("/appointments", async (req, res) => {
+      const appointmentsData = req.body;
+      const result = await appointmentsInfo.insertOne(appointmentsData);
+      res.json(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
